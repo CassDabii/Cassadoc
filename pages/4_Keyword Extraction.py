87e0fd58f32245996630145da081942d676b790e
CassDabi <38007056+CassDabii@users.noreply.github.com>
2023-01-02 19:03:32 +0000

    Added a the code to allow for text summarisation
    Text summarisation added alongside a slider to allow for variance in summary length. Also made the change to load different models based on the algorithm being ran (subject to change if slower performance is a result of this change)

--- pages/4_Keyword Extraction.py
+++ pages/4_Keyword Extraction.py
@@ -4,7 +4,10 @@ import streamlit as st
 import spacy
 from spacy import displacy
 from spacytextblob.spacytextblob import SpacyTextBlob
-
+from spacy.lang.en.stop_words import STOP_WORDS
+from string import punctuation
+from collections import Counter
+from heapq import nlargest
 
 
 
@@ -27,15 +30,14 @@ MAX_FILE_SIZE = 4096 * 4096
 
 if st.session_state['loggedIn'] == True:
 
-    
-    nlp = spacy.load("en_core_web_md")
-
     def extract_named_entities(text):
+        nlp = spacy.load("en_core_web_md")
         doc = nlp(text)
         ent_html = displacy.render(doc, style='ent', jupyter=False)
         st.markdown(ent_html, unsafe_allow_html=True)
 
     def show_sentiment(text):
+        nlp = spacy.load("en_core_web_md")
         nlp.add_pipe(SpacyTextBlob, name='spacytextblob')
         doc = nlp(text)
         doc._.blob.polarity                           
@@ -46,42 +48,84 @@ if st.session_state['loggedIn'] == True:
         st.write('Polarity:', round(doc._.sentiment.polarity, 2))
         st.write('Subjectivity:', round(doc._.sentiment.subjectivity, 2)) 
 
-    def show_summary(text):
-        return
+
+    def show_summary(text,summary_length):
+
+        nlp = spacy.load("en_core_web_sm")
+        doc = nlp(text)
+        len(list(doc.sents))
+        keyword = []
+        stopwords = list(STOP_WORDS)
+        pos_tag = ['PROPN', 'ADJ', 'NOUN', 'VERB']
+        for token in doc:
+            if(token.text in stopwords or token.text in punctuation):
+                continue
+            if(token.pos_ in pos_tag):
+                keyword.append(token.text)
+        freq_word = Counter(keyword)
+        print(freq_word.most_common(5))
+        type(freq_word)#
+        max_freq = Counter(keyword).most_common(1)[0][1]
+        for word in freq_word.keys():  
+            freq_word[word] = (freq_word[word]/max_freq)
+        freq_word.most_common(5)
+
+        sent_strength={}
+        for sent in doc.sents:
+            for word in sent:
+                if word.text in freq_word.keys():
+                    if sent in sent_strength.keys():
+                        sent_strength[sent]+=freq_word[word.text]
+                    else:
+                        sent_strength[sent]=freq_word[word.text]
+
+        # Use the value from the slider to determine the number of sentences in the summary
+        summarized_sentences = nlargest(summary_length, sent_strength, key=sent_strength.get)
+
+        final_sentences = [ w.text for w in summarized_sentences ]
+        summary = ' '.join(final_sentences)
+        st.info(summary)
     
     def extract_keywords(text):
         return
 
     def main():
-        # Get user input
-        text = st.text_area("Enter text or upload a file (PDF or TXT)")
-        file = st.file_uploader("Or select a file (PDF or TXT)", type=["doc", "docx", "pdf", "txt"])
+        with st.spinner("Loading page due to NLP model..."):
+            option = st.radio("Select an option:", ("Named Entity Recognition", "Sentiment Analysis", "Text Summarization", "Keyword Extraction"), horizontal= True)
+            # Get user input
+            text = st.text_area("Enter text or upload a file (PDF or TXT)")
+            file = st.file_uploader("Or select a file (PDF or TXT)", type=["doc", "docx", "pdf", "txt"])
 
-        # Process the text with spaCy
-        if file is not None:
-            text = file.getvalue()
-            text = text.decode("utf-8")
+            # Process the text with spaCy
 
-        # Show options
-        option = st.radio("Select an option:", ("Named Entity Recognition", "Sentiment Analysis", "Text Summarization", "Keyword Extraction"))
-        
-        # Add a submit button
-        if st.button("Submit"):
+            if file is not None:
+                text = file.getvalue()
+                if not file.name.endswith('.txt'):
+                    file.name += '.txt'
+                    text = text.decode("utf-8")
 
-          # Perform selected option
-          if option == "Named Entity Recognition":
-              extract_named_entities(text)
-          elif option == "Sentiment Analysis":
-              show_sentiment(text)
-          elif option == "Text Summarization":
-              show_summary(text)
-          elif option == "Keyword Extraction":
-              extract_keywords(text)
-    if __name__ == "__main__":
+            # Show options
+
+            if option == 'Text Summarization':
+                with st.sidebar:
+                    summary_length = st.slider("Choose the summary length:", min_value=1, max_value=10, value=3)
+
+            # Add a submit button
+            if st.button("Submit"):
+                
+                # Perform selected option
+                if option == "Named Entity Recognition":
+                    extract_named_entities(text)
+                elif option == "Sentiment Analysis":
+                    show_sentiment(text)
+                elif option == "Text Summarization":
+                    show_summary(text,summary_length)
+                elif option == "Keyword Extraction":
+                    extract_keywords(text)
+if __name__ == "__main__":
         main()
 
 
-      
 else:
     st.error("Login Required to use this page")
 
