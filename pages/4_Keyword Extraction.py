7146718ed8186a8d4704b265fd7afe0bb90da710
CassDabi <38007056+CassDabii@users.noreply.github.com>
2023-01-06 16:40:42 +0000

    Added a keyword extraction tool
    This tool allows users to choose how many keywords they want. Plans to show the keywords highlighted in text much like the NER tool were attempted unsuccessfully but will be attempted again in the near future

--- pages/4_Keyword Extraction.py
+++ pages/4_Keyword Extraction.py
@@ -3,14 +3,12 @@ import pyrebase
 import streamlit as st
 import spacy
 from spacy import displacy
-from spacytextblob.spacytextblob import SpacyTextBlob
 from spacy.lang.en.stop_words import STOP_WORDS
 from string import punctuation
 from collections import Counter
 from heapq import nlargest
-from textblob import TextBlob
-from collections import defaultdict
-
+import yake
+import pandas as pd
 
 
 
@@ -39,13 +37,6 @@ if st.session_state['loggedIn'] == True:
         doc = nlp(text)
         ent_html = displacy.render(doc, style='ent', jupyter=False)
         st.markdown(ent_html, unsafe_allow_html=True)
-
-    def show_sentiment(text):
-        # Calculate the sentiment of the text using TextBlob
-        sentiment = TextBlob(text).sentiment
-
-        # Return the polarity and subjectivity as a tuple
-        return sentiment.polarity, sentiment.subjectivity
 
     def show_summary(text,summary_length):
         doc = nlp(text)
@@ -82,32 +73,14 @@ if st.session_state['loggedIn'] == True:
         summary = ' '.join(final_sentences)
         st.info(summary)
     
-    def extract_keywords(text, num_keywords):
-    # Tokenize the text and create a set of stopwords
-        doc = nlp(text)
-        stopwords = set(nlp.Defaults.stop_words)
-
-        # Initialize a defaultdict with a default factory function that returns 0
-        freq_dist = defaultdict(lambda: 0)
-
-        # Iterate through the tokens (words and punctuation) in the document
-        for token in doc:
-            # If the token is a stopword or punctuation, skip it
-            if token.text in stopwords or token.is_punct:
-                continue
-            # If the token has a noun or verb part-of-speech, increment its frequency in the frequency distribution
-            if token.pos_ in ['NOUN', 'VERB']:
-                freq_dist[token.text] += 1
-
-        # Sort the frequency distribution by frequency in descending order
-        sorted_freq_dist = sorted(freq_dist.items(), key=lambda item: item[1], reverse=True)
-
-        # Return the top num_keywords keywords
-        return sorted_freq_dist[:num_keywords]
+    def extract_keywords(text):
+        kw_extractor = yake.KeywordExtractor()
+        keywords = kw_extractor.extract_keywords(text)
+        return keywords
 
     def main():
         with st.spinner("Loading page due to NLP model..."):
-            option = st.radio("Select an option:", ("Named Entity Recognition", "Sentiment Analysis", "Text Summarization", "Keyword Extraction"), horizontal= True)
+            option = st.radio("Select an option:", ("Named Entity Recognition", "Text Summarization", "Keyword Extraction"), horizontal= True)
             # Get user input
             text = st.text_area("Enter text or upload a file (PDF or TXT)")
             file = st.file_uploader("Or select a file (PDF or TXT)", type=["doc", "docx", "pdf", "txt"])
@@ -126,8 +99,7 @@ if st.session_state['loggedIn'] == True:
                 with st.sidebar:
                     summary_length = st.slider("Choose the summary length:", min_value=1, max_value=10, value=3)
             if option == "Keyword Extraction":
-                with st.sidebar:
-                    num_keywords = st.slider("Number of top keywords:", 1, 20, 5)
+                num_keywords = st.slider("Number of top keywords:", 1, 20, 5)
 
             # Add a submit button
             if st.button("Submit"):
@@ -135,27 +107,21 @@ if st.session_state['loggedIn'] == True:
                 # Perform selected option
                 if option == "Named Entity Recognition":
                     extract_named_entities(text)
-                elif option == "Sentiment Analysis":
-                    show_sentiment(text)
-                    polarity, subjectivity = show_sentiment(text)
-                    # Display the sentiment values
-                    st.write("Polarity:", polarity)
-                    st.write("Subjectivity:", subjectivity)
-                    # Add some text to explain what the values mean in more detail
-                    st.success("Polarity is a value from -1 (most negative) to 1 (most positive) that indicates the sentiment of the text. A polarity of -1 means that the text has a negative sentiment,"
-                    + " a polarity of 0 means that the text is neutral or factual, and a polarity of 1 means that the text has a positive sentiment. Subjectivity is a value from 0 (completely objective) "
-                    + "to 1 (completely subjective) that indicates the amount of personal opinion in the text. A subjectivity of 0 means that the text is completely objective and based on facts," + 
-                    "a subjectivity of 0.5 means that the text is balanced between facts and opinions, and a subjectivity of 1 means that the text is completely subjective and based on personal opinions.")
+                
                 elif option == "Text Summarization":
                     show_summary(text,summary_length)
+
                 elif option == "Keyword Extraction":
-                    keywords = extract_keywords(text, num_keywords)
-                    # Output the keywords and their occurrences vertically
-                    for keyword, freq in keywords:
-                        st.info(f"{keyword}: {freq}")
+                    keywords = extract_keywords(text)
+                    for kw in keywords[:num_keywords]:
+                        st.success(kw)
+
+
 if __name__ == "__main__":
-        main()
+    main()
 
 
 else:
     st.error("Login Required to use this page")
+
+
